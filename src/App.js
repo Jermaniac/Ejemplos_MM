e1da07f26964f9a8610ad1504209f37f73bf3a30
javier <javier.diaz-mena@capgemini.com>
2021-05-13 13:40:13 +0200

    Comentario pa ver si esta en la rama

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import { JobFinderContainer } from "../src/JobFinder/containers/JobFinderContainer";
-
+// rama de reduxExample
 export function App() {
   return (
     <JobFinderContainer />

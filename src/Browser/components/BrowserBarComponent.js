61a98b8963e2918de0c9a46ac52f421df8b0429a
Cesco1286 <delussu.fra@gmail.com>
2021-05-13 11:18:23 +0200

    added search bar style

--- src/Browser/components/BrowserBarComponent.js
+++ src/Browser/components/BrowserBarComponent.js
@@ -1,16 +1,18 @@
-import {ResultListContainer} from '../containers/ResultListContainer'
+import { ResultListContainer } from "../containers/ResultListContainer";
 
 export const BrowserBarComponent = (props) => {
+  const { result, handleClick } = props;
 
-    const { result, handleClick } = props;
+  const wrapperStyle = {
+    width: "50%",
+    margin: "auto",
+  };
 
-    return (
-        <>
-            <input type="text" id="browserbar"></input>
-            <button onClick={handleClick}>SEARCH</button>
-            <ResultListContainer busqueda={result}></ResultListContainer>
-        </>
-    );
-
+  return (
+    <div style={wrapperStyle}>
+      <input type="text" id="browserbar"></input>
+      <button onClick={handleClick}>SEARCH</button>
+      <ResultListContainer busqueda={result}></ResultListContainer>
+    </div>
+  );
 };
-
